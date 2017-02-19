94ccbe38ec3ba86e25d0c923bba2f33c56ac13c3
Alex Moore-Niemi <moore.niemi@gmail.com>
2017-02-19 10:53:09 -0500

    fixture pattenr

--- homOS-backend/test/Spec.hs
+++ homOS-backend/test/Spec.hs
@@ -6,6 +6,11 @@ import Lib (app)
 import Test.Hspec
 import Test.Hspec.Wai
 import Test.Hspec.Wai.JSON
+import Data.String.Here
+import Data.Char (isSpace)
+import Data.String (fromString)
+
+fixturize heredoc = fromString $ filter (not . isSpace) heredoc
 
 main :: IO ()
 main = hspec spec
@@ -15,6 +20,6 @@ spec = with (return app) $ do
   describe "GET /commutes/:person" $ do
     it "responds with 200" $ do
       get "/commutes/alex" `shouldRespondWith` 200
-    it "responds with [CommuteStart]" $ do
-      let commutes = "[{\"lat\":\"42.309949\",\"lon\":\"-71.115345\",\"name\":\"775CentreSt\",\"leaving\":\"2017-04-23T18:25:43.511Z\"}]"
+    it "responds with [Commute]" $ do
+      let commutes = fixturize [hereFile|./test/fixtures/commutes.json|]
       get "/commutes/alex" `shouldRespondWith` commutes
